c22e60322068f61807a7957924180f9b0366b750
shinespeciall <648616375@qq.com>
2018-05-21 18:07:54 +0800

    add member functions for level

--- LevelComponents/Door.h
+++ LevelComponents/Door.h
@@ -5,15 +5,24 @@
 class Door
 {
 public:
-    Door::Door(char *doorData);
+    Door(unsigned char _RoomID, unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
     enum LevelComponents::DoorType type;
-    int RoomID;
-    int X1, X2, Y1, Y2;
-    int X_Destination, Y_Destination;
-    int DestinationDoor; // index into currently loaded Door table
-    int X_Displacement, Y_Displacement;
-    int SpriteMapID;
-    int BGM_ID;
+private:
+    unsigned char RoomID;
+    int X1, X2, Y1, Y2; //destination just be (X1, Y1)
+    unsigned char DestinationDoorID; // index into currently loaded Door table
+    unsigned char X_Displacement, Y_Displacement;
+    unsigned char SpriteMapID;
+    unsigned int BGM_ID;
+public:
+    void SetDoorDestination(unsigned char _DestinationDoorID);
+    void SetDoorDisplacement(signed int _X_Displacement, signed int _Y_Displacement);
+    void SetSpriteMapID(unsigned char _SpriteMapID);
+    void SetBGM(unsigned int BGM_ID);
+    void SetDoorPlace(unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
+    void IsUnused();
+    //the following function is provide for resetting the DestinationDoorID value when we delete a door
+    void SpriteMapIDDec();
 };
 
 #endif // DOOR_H
