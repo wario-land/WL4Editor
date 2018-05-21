6650e87ee1326a702d13f0bd0645528d6b2967b0
shinespeciall <648616375@qq.com>
2018-05-22 01:45:32 +0800

    implement door class

--- LevelComponents/Door.cpp
+++ LevelComponents/Door.cpp
@@ -1,13 +1,52 @@
 #include "Door.h"
 
-namespace LevelComponents
+LevelComponents::Door::Door(unsigned char _RoomID, LevelComponents::DoorType _DoorType, unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2)
 {
-    enum DoorType
-    {
-        Portal  = 1,
-        Instant = 2,
-        Normal  = 3,
-        TYPE_04 = 4,
-        TYPE_05 = 5
-    };
+    this->RoomID = _RoomID;
+    this->type = _DoorType;
+    this->X1 = _X1;
+    this->X2 = _X2;
+    this->Y1 = _Y1;
+    this->Y2 = _Y2;
+}
+
+void LevelComponents::Door::SetDoorDestination(unsigned char _DestinationDoorID)
+{
+    this->DestinationDoorID = _DestinationDoorID;
+}
+
+void LevelComponents::Door::SetDoorDisplacement(signed int _X_Displacement, signed int _Y_Displacement)
+{
+    this->X_Displacement = (unsigned char)(255-16*_X_Displacement);
+    this->Y_Displacement = (unsigned char)(255-16*_Y_Displacement);
+}
+
+void LevelComponents::Door::SetSpriteMapID(unsigned char _SpriteMapID)
+{
+    this->SpriteMapID = _SpriteMapID;
+}
+
+void LevelComponents::Door::SetBGM(unsigned int _BGM_ID)
+{
+    this->BGM_ID = _BGM_ID;
+}
+
+void LevelComponents::Door::SetDoorPlace(unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2)
+{
+    this->X1 = _X1;
+    this->X2 = _X2;
+    this->Y1 = _Y1;
+    this->Y2 = _Y2;
+}
+
+bool LevelComponents::Door::IsUnused()
+{
+    if((this->type != Portal) && (this->DestinationDoorID == (unsigned char)'\x00'))
+        return true;
+    return false;
+}
+
+void LevelComponents::Door::SpriteMapIDDec()
+{
+    this->DestinationDoorID = this->DestinationDoorID - 1;
 }
--- LevelComponents/Door.h
+++ LevelComponents/Door.h
@@ -1,28 +1,38 @@
 #ifndef DOOR_H
 #define DOOR_H
 
-
-class Door
+namespace LevelComponents
 {
-public:
-    Door(unsigned char _RoomID, unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
-    enum LevelComponents::DoorType type;
-private:
-    unsigned char RoomID;
-    int X1, X2, Y1, Y2; //destination just be (X1, Y1)
-    unsigned char DestinationDoorID; // index into currently loaded Door table
-    unsigned char X_Displacement, Y_Displacement;
-    unsigned char SpriteMapID;
-    unsigned int BGM_ID;
-public:
-    void SetDoorDestination(unsigned char _DestinationDoorID);
-    void SetDoorDisplacement(signed int _X_Displacement, signed int _Y_Displacement);
-    void SetSpriteMapID(unsigned char _SpriteMapID);
-    void SetBGM(unsigned int BGM_ID);
-    void SetDoorPlace(unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
-    void IsUnused();
-    //the following function is provide for resetting the DestinationDoorID value when we delete a door
-    void SpriteMapIDDec();
-};
+    enum DoorType
+    {
+        Portal  = 1,
+        Instant = 2,
+        Normal  = 3,
+        TYPE_04 = 4,
+        TYPE_05 = 5
+    };
 
+    class Door
+    {
+    public:
+        Door(unsigned char _RoomID, enum LevelComponents::DoorType _DoorType, unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
+    private:
+        enum LevelComponents::DoorType type;
+        unsigned char RoomID;
+        int X1, X2, Y1, Y2; //destination just be (X1, Y1)
+        unsigned char DestinationDoorID; // index into currently loaded Door table
+        unsigned char X_Displacement, Y_Displacement;
+        unsigned char SpriteMapID;
+        unsigned int BGM_ID;
+    public:
+        void SetDoorDestination(unsigned char _DestinationDoorID);
+        void SetDoorDisplacement(signed int _X_Displacement, signed int _Y_Displacement);
+        void SetSpriteMapID(unsigned char _SpriteMapID);
+        void SetBGM(unsigned int _BGM_ID);
+        void SetDoorPlace(unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
+        bool IsUnused();
+        //the following function is provide for resetting the DestinationDoorID value when we delete a door
+        void SpriteMapIDDec();
+    };
+}
 #endif // DOOR_H
