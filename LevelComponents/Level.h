0658c8b5b923e76916287d00282cb9fadd2c29de
shinespeciall <648616375@qq.com>
2018-05-23 19:53:27 +0800

    add more details
    1. fix some problem in Door class
    2. add some TODOs
    3. add __LevelHeader sturct
    3. implement Level::SetTimeCountdownCounter()
    4. some other things

--- LevelComponents/Level.h
+++ LevelComponents/Level.h
@@ -8,14 +8,33 @@
 
 namespace LevelComponents
 {
+    enum __LevelDifficulty{Normal = 0, Hard = 1, SHard = 2};
+    struct __LevelHeader
+    {
+        unsigned char HeaderPointerIndex;   //multiply 4 make a shift from some base pointers
+        unsigned char NumOfMap;    //star from 1 so it's okey to initial it by 0
+        unsigned char Unknown0A;   //always 0x0A
+        unsigned char HardModeMinuteNum;
+        unsigned char HardModeSecondTenPlaceNum;
+        unsigned char HardModeSecondOnePlaceNum;
+        unsigned char NormalModeMinuteNum;
+        unsigned char NormalModeSecondTenPlaceNum;
+        unsigned char NormalModeSecondOnePlaceNum;
+        unsigned char SHardModeMinuteNum;
+        unsigned char SHardModeSecondTenPlaceNum;
+        unsigned char SHardModeSecondOnePlaceNum;
+    };
+
     class Level
     {
     private:
         std::vector<Room> rooms;
         std::string LevelName;
         std::vector<Door*> doors;
+        __LevelHeader LevelHeader;
     public:
-        Level(int passage, int stage);
+        Level(int passage, int stage); //TODO: need another construction function ? one for load a level and the other for create a level (ssp)
+        void SetTimeCountdownCounter(enum __LevelDifficulty LevelDifficulty, unsigned char _MinuteNum, unsigned char _SecondTenPlaceNum, unsigned char _OnePlaceNum);// TODO void or bool ? (ssp)
     };
 }
 
--- WL4Constants.h
+++ WL4Constants.h
@@ -6,6 +6,7 @@ namespace WL4Constants
     const int LevelHeaderIndexTable = 0x6391C4;
     const int AreaDataTable = 0x78F280;
     const int DoorTable = 0x78F21C;
+    const int LevelNamePointerTable = 0x63A3AC;
 }
 
 #endif // WL4CONSTANTS_H
