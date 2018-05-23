2bdf2a835084ed3c463bc46d7824d0588144f5e7
Andrew Wilder <andrew.m.wilder@gmail.com>
2018-05-22 20:22:53 -0700

    Updating the Door class

--- LevelComponents/Door.h
+++ LevelComponents/Door.h
@@ -1,31 +1,37 @@
 #ifndef DOOR_H
 #define DOOR_H
 
+#include <memory>
+
 namespace LevelComponents
 {
+    // An enumeration of the types of doors in the game.
+    // The integers correspond to the real values the games uses internally to represent a door.
     enum DoorType
     {
-        Portal  = 1,
-        Instant = 2,
-        Normal  = 3,
-        TYPE_04 = 4,
-        TYPE_05 = 5
+        Portal  = 1, // A special type just for the level's portal
+        Instant = 2, // Automatically "enter" the door. For example, the edge of a room leading to another one
+        Normal  = 3, // A door with normal "door"-like behavior. Press up to enter
+        TYPE_04 = 4, // unknown
+        TYPE_05 = 5  // unknown
     };
 
     class Door
     {
     public:
+        Door(unsigned char *dataPtr);
         Door(unsigned char _RoomID, enum LevelComponents::DoorType _DoorType, unsigned char _X1, unsigned char _X2, unsigned char _Y1, unsigned char _Y2);
     private:
         enum LevelComponents::DoorType type;
         unsigned char RoomID;
         int X1, X2, Y1, Y2; //destination just be (X1, Y1)
-        unsigned char DestinationDoorID; // index into currently loaded Door table
+        std::shared_ptr<Door*> DestinationDoor;
         unsigned char X_Displacement, Y_Displacement;
         unsigned char SpriteMapID;
         unsigned int BGM_ID;
     public:
-        void SetDoorDestination(unsigned char _DestinationDoorID);
+        Door *GetDestinationDoor();
+        void SetDestinationDoor(Door *otherDoor);
         void SetDoorDisplacement(signed int _X_Displacement, signed int _Y_Displacement);
         void SetSpriteMapID(unsigned char _SpriteMapID);
         void SetBGM(unsigned int _BGM_ID);
@@ -36,4 +42,5 @@ namespace LevelComponents
         void DestinationDoorIDDec();
     };
 }
+
 #endif // DOOR_H
