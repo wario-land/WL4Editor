aba4d63208d72e39359945a71b36728765bae4a0
shinespeciall <648616375@qq.com>
2018-05-31 20:32:25 +0800

    implement function RLEDecompress()

--- ROMUtils.h
+++ ROMUtils.h
@@ -13,6 +13,7 @@ namespace ROMUtils
     int IntFromData(unsigned char *data, int address);
     int PointerFromData(unsigned char *data, int address);
     // TODO RLE decompress
+    unsigned char *RLEDecompress(unsigned char *data, int address, int outputSize);
 }
 
 #endif // ROMUTILS_H
